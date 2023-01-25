2643931bdea6fed20461139046e5e6c5d5af3336
Mustapha David <mustaphadavid98@gmail.com>
2023-01-25 23:25:13 +0100

    all

--- lib/Pages/HomePages/home_page_1.dart
+++ lib/Pages/HomePages/home_page_1.dart
@@ -4,8 +4,9 @@ import 'package:food_delivery/Pages/shimmer_page.dart';
 import 'package:food_delivery/models/images_list.dart';
 import 'package:food_delivery/widgets/colors.dart';
 import 'package:food_delivery/widgets/navbar.dart';
-
+import 'package:http/http.dart' as http;
 import '../../widgets/food_main_page.dart';
+import 'dart:convert';
 
 class HomePage1 extends StatefulWidget {
   final newListOfItems = listOfItems();
@@ -19,8 +20,24 @@ class _HomePage1State extends State<HomePage1> {
   void initState(){
     super.initState();
     loadData();
+    callApi();
   }
   bool? isLoading;
+  String? stringResponse;
+  Map? mapResponse;
+  Map? dataResponse;
+  Future callApi() async{
+    http.Response response;
+   response = await http.get(Uri.parse("https://reqres.in/api/users/2"));
+   if (response.statusCode == 200) {
+     setState(() {
+       // stringResponse = response.body;
+       mapResponse = json.decode(response.body);
+       // dataResponse = mapResponse!['text'];
+     });
+   }
+   print(response.body);
+  }
   Future loadData() async {
     setState(() => isLoading = true);
     await Future.delayed(Duration(seconds: 1), (){});
@@ -35,8 +52,9 @@ class _HomePage1State extends State<HomePage1> {
       appBar: AppBar(
         toolbarHeight: 90.0,
         iconTheme: IconThemeData(color: ColorsToBeUsed().primaryColor),
-        title: Text(
-          'Welcome to Foodco',
+        title: mapResponse == null ? const Text('data is null') : Text(
+          mapResponse!['text'].toString(),
+          // 'Welcome to Foodco',
           style: TextStyle(
             fontFamily: 'poppins',
             fontWeight: FontWeight.w300,
